763b39b2211270c9e81db5a7f784e165132a5712
Valeria Bobko <bobko356@ya.ru>
2021-12-06 16:53:43 +0300

    Initial commit

---
+++ src/main/java/ru/buildservice/BuildserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.buildservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/buildservice/BuildserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.buildservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
