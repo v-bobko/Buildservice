78fe0ab631bc720fa5733d12da57771aacfe096f
Valeria Bobko <bobko356@ya.ru>
2022-02-11 18:05:06 +0300

    контроллер рабочего: добавление записей в журнал, отображение проектов, шапка личного кабинета

--- src/main/java/ru/buildservice/project/controller/WorkerController.java
+++ src/main/java/ru/buildservice/project/controller/WorkerController.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import ru.buildservice.project.Datetime;
 import ru.buildservice.project.entity.*;
-import ru.buildservice.project.repository.CalendarServiceRepository;
-import ru.buildservice.project.repository.ObjectRepository;
-import ru.buildservice.project.repository.PhotoRepository;
-import ru.buildservice.project.repository.UserRepository;
+import ru.buildservice.project.repository.*;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,22 +30,26 @@ public class WorkerController {
     private ObjectRepository objectRepository;
     @Autowired
     private PhotoRepository photoRepository;
+    @Autowired
+    private JournalRepository journalRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
 
-//  Страница с объектами
+    //  Страница с объектами
     @GetMapping("/worker/objects")
     public String workerObjects(Model model) {
 
 
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            Users user = userRepository.findByUsername(auth.getName());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
 
-            List<Integer> userID = calendarServiceRepository.findObjectbyUserId(user.getUser_id());
+        List<Integer> userID = calendarServiceRepository.findObjectbyUserId(user.getUser_id());
 
-            List<Objects> object= new ArrayList<>();
-            userID.forEach(us ->object.add(objectRepository.findByObjectId(us)));
+        List<Objects> object = new ArrayList<>();
+        userID.forEach(us -> object.add(objectRepository.findByObjectId(us)));
 
-            model.addAttribute("user", user);
-            model.addAttribute("objects", object);
+        model.addAttribute("user", user);
+        model.addAttribute("objects", object);
 
         return "worker/worker-objects";
     }
@@ -55,39 +57,38 @@ public class WorkerController {
 
     @GetMapping("/worker/work/{id}")
     public String workerWork(@PathVariable(value = "id") int id, @RequestParam(required = false, value = "month") String curMonth, @RequestParam(required = false, value = "year") Integer curYear, Model model) {
-            Objects object = objectRepository.findById(id).orElseThrow();
-            model.addAttribute("object", object);
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            Users user = userRepository.findByUsername(auth.getName());
-            model.addAttribute("user",user);
-            ArrayList<Integer> years = calendarServiceRepository.findYears();
-            model.addAttribute("years", years);
-            String month;
-            Integer year;
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+        model.addAttribute("user", user);
+        ArrayList<Integer> years = calendarServiceRepository.findYears();
+        model.addAttribute("years", years);
+        String month;
+        Integer year;
 
-            if (curMonth == null) {
-                Datetime datetime = new Datetime();
-                month = datetime.extractMonth();
-                year = datetime.extractYear();
-            } else {
-                year=curYear;
-                month=curMonth;
-            }
-
-
-            model.addAttribute("month", month);
-            model.addAttribute("year", year);
-
-            List<CalendarService> calendarServices1 = calendarServiceRepository.findByObjectsAndMonthAndYearAndUsersOrderByCalendarIdAsc(object, month, year, user);
-            model.addAttribute("calendar", calendarServices1);
-
-            List<Photo> photo = photoRepository.findByObjectsAndMonthAndYearOrderByPhotoIdDesc(object,month,year);
-            model.addAttribute("photo", photo);
-
-
-                return "worker/worker-work";
-
+        if (curMonth == null) {
+            Datetime datetime = new Datetime();
+            month = datetime.extractMonth();
+            year = datetime.extractYear();
+        } else {
+            year = curYear;
+            month = curMonth;
         }
+
+
+        model.addAttribute("month", month);
+        model.addAttribute("year", year);
+
+        List<CalendarService> calendarServices1 = calendarServiceRepository.findByObjectsAndMonthAndYearAndUsersOrderByCalendarIdAsc(object, month, year, user);
+        model.addAttribute("calendar", calendarServices1);
+
+        List<Photo> photo = photoRepository.findByObjectsAndMonthAndYearOrderByPhotoIdDesc(object, month, year);
+        model.addAttribute("photo", photo);
+
+        return "worker/worker-work";
+
+    }
 
 
     @PostMapping("/worker/work/addphoto/{id}")
@@ -98,10 +99,6 @@ public class WorkerController {
         photo.setMonth(month);
         photo.setYear(year);
 
-
-
-
-
         photoRepository.save(photo);
 
         CalendarComment calendarComment = new CalendarComment();
@@ -110,38 +107,77 @@ public class WorkerController {
         calendarComment.setMonth(month);
         calendarComment.setObjects(objects);
 
-
-
         return "worker/worker-work";
     }
 
+    @GetMapping("/worker/psd/{id}")
+    public String workerPSD(@PathVariable(value = "id") int id, Model model) {
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+        model.addAttribute("user", user);
 
+        List<Projects> projects = projectRepository.findByObjects(object);
+        model.addAttribute("projects", projects);
 
-
-
-
-
-
-
-
-
-
-
-
-    @GetMapping("/worker/psd")
-    public String workerPSD() {
         return "worker/worker-psd";
     }
 
 
+    @GetMapping("/worker/journal/{id}")
+    public String workerJournal(@PathVariable(value = "id") int id, Model model) {
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+        model.addAttribute("user", user);
 
-    @GetMapping("/worker/journal")
-    public String workerJournal() {
+        List<Journal> journals = journalRepository.findByObjectsAndUsersOrderByDateDescJournalIdDesc(object, user);
+        model.addAttribute("journal", journals);
+
         return "worker/worker-journal";
     }
 
-    @GetMapping("/worker/application")
-    public String workerApplication() {
+    @PostMapping("/worker/journal/{id}/addStr")
+    public String addStringWorker(@PathVariable(value = "id") int id,
+                                  @RequestParam(value = "date") Date date,
+                                  @RequestParam(value = "fio") String fio,
+                                  @RequestParam(value = "time") Integer time,
+                                  @RequestParam(value = "report") String report,
+                                  @RequestParam(value = "userId") Integer userId) {
+
+        Objects object = objectRepository.findById(id).orElseThrow();
+        Users user=userRepository.findById(userId).orElseThrow();
+
+        Journal journal=new Journal(date,fio,time,report,object,user);
+        journalRepository.save(journal);
+
+        return "redirect:/worker/journal/{id}";
+    }
+
+    @PostMapping("/worker/journal/{id}/dellStr")
+    public String dellStringWorker(@PathVariable(value = "id") int id,Model model) {
+
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+
+        Journal journal=journalRepository.findLastJournalByUsers(user.getUser_id());
+        journalRepository.delete(journal);
+
+        return "redirect:/worker/journal/{id}";
+    }
+
+    @GetMapping("/worker/application/{id}")
+    public String workerApplication(@PathVariable(value = "id") int id, Model model) {
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+        model.addAttribute("user", user);
+
         return "worker/worker-application";
     }
 
--- src/main/java/ru/buildservice/project/repository/JournalRepository.java
+++ src/main/java/ru/buildservice/project/repository/JournalRepository.java
@@ -1,13 +1,22 @@
 package ru.buildservice.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.User;
 import ru.buildservice.project.entity.CalendarService;
 import ru.buildservice.project.entity.Journal;
 import ru.buildservice.project.entity.Objects;
+import ru.buildservice.project.entity.Users;
 
 import java.util.List;
 
 public interface JournalRepository extends JpaRepository<Journal,Integer> {
 
-    List<Journal> findByObjects(Objects object);
+    List<Journal> findByObjectsOrderByDateDescJournalIdDesc(Objects object);
+
+    List<Journal> findByObjectsAndUsersOrderByDateDescJournalIdDesc(Objects object, Users user);
+
+    @Query(nativeQuery = true, value="SELECT * FROM journal WHERE worker_id=:id ORDER BY journal_id DESC LIMIT 1")
+    Journal findLastJournalByUsers(@Param("id") Integer workerId);
 }
