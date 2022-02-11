976903c56854850ee9d54e174898349f279e70f0
Anton Iliushin <iliushin.a.g@gmail.com>
2022-02-11 18:05:41 +0300

    Добавлено множественная загрузка фото

--- src/main/java/ru/buildservice/project/controller/WorkerController.java
+++ src/main/java/ru/buildservice/project/controller/WorkerController.java
@@ -2,6 +2,7 @@ package ru.buildservice.project.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -18,8 +19,13 @@ import ru.buildservice.project.repository.ObjectRepository;
 import ru.buildservice.project.repository.PhotoRepository;
 import ru.buildservice.project.repository.UserRepository;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 
 @Controller
@@ -32,107 +38,121 @@ public class WorkerController {
     private ObjectRepository objectRepository;
     @Autowired
     private PhotoRepository photoRepository;
+@Value("${filePathPhoto}")
+   private String filePathPhoto;
 
-//  Страница с объектами
+    //  Страница с объектами
     @GetMapping("/worker/objects")
     public String workerObjects(Model model) {
 
 
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            Users user = userRepository.findByUsername(auth.getName());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
 
-            List<Integer> userID = calendarServiceRepository.findObjectbyUserId(user.getUser_id());
+        List<Integer> userID = calendarServiceRepository.findObjectbyUserId(user.getUser_id());
 
-            List<Objects> object= new ArrayList<>();
-            userID.forEach(us ->object.add(objectRepository.findByObjectId(us)));
+        List<Objects> object = new ArrayList<>();
+        userID.forEach(us -> object.add(objectRepository.findByObjectId(us)));
 
-            model.addAttribute("user", user);
-            model.addAttribute("objects", object);
+        model.addAttribute("user", user);
+        model.addAttribute("objects", object);
 
         return "worker/worker-objects";
     }
 
 
     @GetMapping("/worker/work/{id}")
-    public String workerWork(@PathVariable(value = "id") int id, @RequestParam(required = false, value = "month") String curMonth, @RequestParam(required = false, value = "year") Integer curYear, Model model) {
-            Objects object = objectRepository.findById(id).orElseThrow();
-            model.addAttribute("object", object);
-            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-            Users user = userRepository.findByUsername(auth.getName());
-            model.addAttribute("user",user);
-            ArrayList<Integer> years = calendarServiceRepository.findYears();
-            model.addAttribute("years", years);
-            String month;
-            Integer year;
+    public String workerWork(@PathVariable(value = "id") int id,
+                             @RequestParam(required = false, value = "month") String curMonth,
+                             @RequestParam(required = false, value = "year") Integer curYear, Model model) {
+        Objects object = objectRepository.findById(id).orElseThrow();
+        model.addAttribute("object", object);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = userRepository.findByUsername(auth.getName());
+        model.addAttribute("user", user);
+        ArrayList<Integer> years = calendarServiceRepository.findYears();
+        model.addAttribute("years", years);
+        String month;
+        Integer year;
 
-            if (curMonth == null) {
-                Datetime datetime = new Datetime();
-                month = datetime.extractMonth();
-                year = datetime.extractYear();
-            } else {
-                year=curYear;
-                month=curMonth;
-            }
-
-
-            model.addAttribute("month", month);
-            model.addAttribute("year", year);
-
-            List<CalendarService> calendarServices1 = calendarServiceRepository.findByObjectsAndMonthAndYearAndUsersOrderByCalendarIdAsc(object, month, year, user);
-            model.addAttribute("calendar", calendarServices1);
-
-            List<Photo> photo = photoRepository.findByObjectsAndMonthAndYearOrderByPhotoIdDesc(object,month,year);
-            model.addAttribute("photo", photo);
-
-
-                return "worker/worker-work";
-
+        if (curMonth == null) {
+            Datetime datetime = new Datetime();
+            month = datetime.extractMonth();
+            year = datetime.extractYear();
+        } else {
+            year = curYear;
+            month = curMonth;
         }
 
 
-    @PostMapping("/worker/work/addphoto/{id}")
-    public String addPhoto(@PathVariable(value = "id") int id, @RequestParam(value = "photo") MultipartFile file, @RequestParam(value = "month") String month, @RequestParam(value = "year") Integer year, Model model) {
-        Objects objects = objectRepository.findById(id).orElseThrow();
-        Photo photo = new Photo();
-        photo.setObjects(objects);
-        photo.setMonth(month);
-        photo.setYear(year);
+        model.addAttribute("month", month);
+        model.addAttribute("year", year);
 
+        List<CalendarService> calendarServices1 = calendarServiceRepository.findByObjectsAndMonthAndYearAndUsersOrderByCalendarIdAsc(object, month, year, user);
+        model.addAttribute("calendar", calendarServices1);
 
-
-
-
-        photoRepository.save(photo);
-
-        CalendarComment calendarComment = new CalendarComment();
-        calendarComment.setYear(year);
-
-        calendarComment.setMonth(month);
-        calendarComment.setObjects(objects);
-
+        List<Photo> photo = photoRepository.findByObjectsAndMonthAndYearOrderByPhotoIdDesc(object, month, year);
+        model.addAttribute("photo", photo);
 
 
         return "worker/worker-work";
+
     }
 
 
+    @PostMapping("/worker/work/addphoto/{id}")
+    public String addPhoto(@PathVariable(value = "id") int id,
+                           @RequestParam(value = "photo") MultipartFile[] files,
+                           @RequestParam(value = "month") String month,
+                           @RequestParam(value = "year") Integer year, Model model) throws IOException {
+
+        Objects object = objectRepository.findById(id).orElseThrow();
+
+
+        for (MultipartFile file: files
+             ) {
 
 
 
+        if (!file.isEmpty()) {
+
+            String dotExtendName = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));// Получить расширение
+            String fileName = UUID.randomUUID().toString().replace("-", "") + dotExtendName;// Имя UUID + расширение.
 
 
 
+            File path = new File(filePathPhoto);
+            if (!path.exists()) {
+                path.mkdirs();
+            }
+            // Загрузить
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(filePathPhoto + fileName)));
+            out.write(file.getBytes());
+            out.flush();
+            out.close();
+
+            String filenameBD = "/images/" + fileName;
+
+            Photo photo = new Photo();
+            photo.setObjects(object);
+            photo.setMonth(month);
+            photo.setYear(year);
+            photo.setPhoto(filenameBD);
+            photoRepository.save(photo);
+            photoRepository.save(photo);
+        }}
 
 
 
-
+        model.addAttribute("object",object);
+        return "redirect:/worker/work/{id}";
+    }
 
 
     @GetMapping("/worker/psd")
     public String workerPSD() {
         return "worker/worker-psd";
     }
-
 
 
     @GetMapping("/worker/journal")
